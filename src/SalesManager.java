3b680fbed455af136669bf6e1ac2b2fe5899e7f5
Ibragim Esavulov <esavulov@mail.ru>
2022-09-11 13:25:30 +0300

    Initial commit

---
+++ src/SalesManager.java
@@ -0,0 +1,17 @@
+public class SalesManager {
+    protected long[] sales;
+
+    public SalesManager(long[] sales) {
+        this.sales = sales;
+    }
+
+    public long max() {
+        long max = -1;
+        for (long sale : sales) {
+            if (sale > max) {
+                max = sale;
+            }
+        }
+        return max;
+    }
+}
